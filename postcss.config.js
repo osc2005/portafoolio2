8390a4a67e7304b8cb19ca689acc83337a73ede2
arepitacaliente! <oscar.aroca@cambridgehn.com>
2026-03-06 01:14:20 -0600

    Primer commit portafolio vite

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+import purgecss from '@fullhuman/postcss-purgecss'
+
+export default {
+  plugins: [
+    purgecss({
+      content: ['./index.html', './src/**/*.js'],
+      defaultExtractor: (content) =>
+        content.match(/[\w-/:%.]+(?<!:)/g) || []
+    })
+  ]
+}
